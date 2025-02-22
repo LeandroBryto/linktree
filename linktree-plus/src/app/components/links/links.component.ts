f3986ee4549e303e3b8fa47bd13d1d744518f18c
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-02-22 18:51:18 -0300

    mapeamento de páginas
     URLs e no mapeamento de páginas

--- linktree-plus/src/app/components/links/links.component.ts
+++ linktree-plus/src/app/components/links/links.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { LinkService } from './../../services/link.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-links',
@@ -6,6 +7,13 @@ import { Component } from '@angular/core';
   templateUrl: './links.component.html',
   styleUrl: './links.component.scss'
 })
-export class LinksComponent {
+export class LinksComponent implements OnInit{
+  links:any =[];
+  constructor(private LinkService: LinkService){}
 
+  ngOnInit(): void {
+   this.LinkService.getLinks().then((links) =>{
+      this.links = links.filter(link => link.isVisible);
+   });
+  }
 }
--- linktree-plus/src/app/services/link.service.ts
+++ linktree-plus/src/app/services/link.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { defineOneEntry } from 'oneentry';
 import OrdersApi from 'oneentry/dist/orders/ordersApi';
+import { Link } from '../types/link.type';
 
 const ONEENTRY_URL = 'https://linktree-plus.oneentry.cloud';
 const ONEENTRY_TOKEN ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiYW5ndWxhci1mcm9udGVuZCIsInNlcmlhbE51bWJlciI6MSwiaWF0IjoxNzQwMTc4Nzc2LCJleHAiOjE3NzE3MTQ2Nzl9.FPlKTjkq2uOGYyccCAsBiQ3WFl_fAQ8ClBGDiiH5N9w'
@@ -21,16 +22,26 @@ export class LinkService {
 
   constructor() { }
 
-  async getLinks(): Promise<Page[]>{
+  async getLinks(): Promise<Link[]>{
+    try{
     let pages = await Pages.getPages();
-    return pages.map((page) => {
-      const pageExistssOutside = page.attributeValues?.['pages-exists-outside']?.value || false;
+    return pages.map((page: any) => {
+      const pageExistssOutside = page.attributeValues?.['page-exists-outside']?.value || false;
+
+      let extractedUrl = page.pageUrl;
+      if(pageExistssOutside){
       const urlFormatted = page.localizeInfos?.['htmlContent']?.replace(/^<p>|<\/p>$/g,'') || '';
+      const extractedUrl = urlFormatted.match(/https?:\/\/[^\s"<>]+/)[0];
+      }
       return{
         title: page.localizeInfos?.['title'] || '',
         isVisible: page.isVisible || true,
-        url:pageExistssOutside ? urlFormatted : page.pageUrl
-      }
-    });
+        url: extractedUrl
+        }
+     });
+    } catch (error){
+      console.log("Error ao buscar links")
+     return []
+    }
   }
 }
